9dcbff0547bf2b2d6d9542d7f34befbef5b49beb
420MemesxD <55286185+420MemesxD@users.noreply.github.com>
2020-03-21 13:58:04 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
 let requestURL = 'https://420memesxd.github.io/JS-Lab4/main.json';
 
-var request = new XMLHttpRequest();
+let request = new XMLHttpRequest();
 request.open('GET', requestURL);
 request.responseType = 'json';
 request.send();
 
 request.onload = function(){
-  let strangeProducts = request.response;
-  console.log(strangeProducts);
-  sProducts(strangeProducts);
+  let products = request.response;
+  console.log(products);
+  sProducts(products);
 };
 
 function sProducts(jsonObj){
 
-  let sProducts = jsonObj.main;
+  let sProducts = jsonObj.strangeProducts;
 
   for(let i = 0; i < sProducts.length; i++){
 
@@ -24,21 +24,20 @@ function sProducts(jsonObj){
   let p = document.createElement('p');
   let ul = document.createElement('ul');
 
-  let section = document.quuerySelector('section');
-  img.setAttribute('src', 'https://420memesxd.github.io/JavaScript/assets/' + sProducts[i].image);
-  img.setAttribute('alt', sProducts[i].image);
+  let section = document.querySelector('section');
   h2.textContent = sProducts[i].name;
-  p1.textContent = 'Price' + sProducts[i].price;
-  let toppings = sProducts[i].toppings;
+  img.setAttribute('src', 'https://420memesxd.github.io/JS-Lab4/assets/' + sProducts[i].image);
+  img.setAttribute('alt', sProducts[i].image);
+  p.textContent = 'Price' + sProducts[i].price;
+  let details = sProducts[i].details;
   for (let j = 0; j < details.length; j++){
     let listItem = document.createElement('li');
     listItem.textContent = details[j];
     ul.appendChild(listItem);
   }
+article.appendChild(h2);
 article.appendChild(img);
-article.appendChild(h1);
-article.appendChild(p1);
-article.appendChild(p2);
+article.appendChild(p);
 article.appendChild(ul);
 section.appendChild(article);
   }
